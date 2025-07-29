45e7e64823baa14d560118b7f11d379faf127936
Yaroslav <yaroslavgolinsiy@gmail.com>
2025-07-29 13:01:01 +0700

    added ImageManagerPro

--- PaymentProjectMVVM/ImageFlow/ImageManager.swift
+++ PaymentProjectMVVM/ImageFlow/ImageManager.swift
@@ -2,14 +2,26 @@
 import Dependencies
 import Foundation
 import SwiftUI
+import UIKit
 
 protocol ImageApiCompletion {
     func downloadImage(url: URL, completion: @escaping(Result<UIImage, Error>) -> Void)
     func downloadImages(urls: [URL], completion: @escaping(Result<[UIImage], Error>) -> Void)
 }
 
+enum ImageApiCompletionKey: DependencyKey {
+    static let liveValue: ImageApiCompletion = ImageManager()
+}
+
+extension DependencyValues {
+    var imageManager: ImageApiCompletion {
+        get  { self[ImageApiCompletionKey.self] }
+        set  { self[ImageApiCompletionKey.self] = newValue }
+    }
+}
 
 struct ImageManager: ImageApiCompletion {
+    
     func downloadImage(url: URL, completion: @escaping(Result<UIImage, Error>) -> Void){
         
         let task = URLSession.shared.dataTask(with: url) { data, _, error in
@@ -50,18 +62,81 @@ struct ImageManager: ImageApiCompletion {
             }
         }
     }
-
-    
 }
 
-
-enum ImageApiCompletionKey: DependencyKey {
-    static let liveValue: ImageApiCompletion = ImageManager()
+enum ImageApiCompletionProKey: DependencyKey {
+    static let liveValue: ImageApiCompletionPro = ImageManagerPro()
 }
 
 extension DependencyValues {
-    var imageManager: ImageApiCompletion {
-        get  { self[ImageApiCompletionKey.self] }
-        set  { self[ImageApiCompletionKey.self] = newValue }
+    var imageProManager: ImageApiCompletionPro {
+        get  { self[ImageApiCompletionProKey.self] }
+        set  { self[ImageApiCompletionProKey.self] = newValue }
     }
+}
+
+protocol ImageApiCompletionPro {
+    func downloadImage(url: URL, completion: @escaping(Result<UIImage, Error>) -> Void)
+    func downloadImages(urls:[URL], completion: @escaping(Result<[UIImage], Error>) -> Void)
+}
+
+
+class ImageManagerPro: ImageApiCompletionPro {
+    
+    private var downloadedImages: [URL: UIImage] = [:]
+    private let dispachQueue = DispatchQueue.init(label: "DispatchQueue", attributes: .concurrent)
+    private let dispatchSemaphore = DispatchSemaphore(value: 3)
+    private var dataTasks: [URL: URLSessionDataTask] = [:]
+    
+    func downloadImage(url: URL, completion: @escaping(Result<UIImage, Error>) -> Void) {
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            
+            guard let self = self else { return }
+            
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data, let image = UIImage(data: data) else {
+                completion(.failure(NSError(domain: "", code: 1, userInfo: [NSLocalizedDescriptionKey : "Failure image"])))
+                return
+            }
+            
+            downloadedImages[url] = image
+            completion(.success(image))
+        }
+        task.resume()
+    }
+    
+    func downloadImages(urls:[URL], completion: @escaping(Result<[UIImage], Error>) -> Void) {
+        let dispachGroup = DispatchGroup()
+        var images: [UIImage] = []
+        
+        for url in urls {
+            
+            dispachGroup.enter()
+            dispatchSemaphore.wait()
+            
+            dispachQueue.async {
+                self.downloadImage(url: url) { [weak self] result in
+                    
+                    dispachGroup.leave()
+                    self?.dispatchSemaphore.signal()
+                    
+                    if case .success(let success) = result {
+                        images.append(success)
+                    }
+                }
+            }
+        }
+        
+        dispachGroup.notify(queue: .main) {
+            completion(.success(images))
+        }
+        
+       
+    }
+    
 }
